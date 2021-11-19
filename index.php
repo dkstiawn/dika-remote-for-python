49f57d0f579334de3cc3f13f554b85cb759cfa64
Dika Setiawan <dikaleovarista@gmail.com>
2021-11-19 22:41:34 +0700

    Mengubah file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Coba Git </title>
+	<style type="text/css">
+		body {
+				font-family: Times new roman;
+	}
+	</style>
+</head>
+<body>
+	<h1> Hello Wolrd </h1>
+<script type="text/javascript" src="script.js"></script>	
+</body>
+</html>
